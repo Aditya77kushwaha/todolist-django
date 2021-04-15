b725d0a537f48b289279957beff08702c8e01187
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-04-15 14:00:18 +0530

    Did frontend, migrations, admin conf., send data from db.

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from . models import MyModel
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, "index.html", {})
+    todos = MyModel.objects.all()
+    return render(request, "index.html", {"data": todos})
