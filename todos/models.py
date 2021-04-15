141320dc9b012cad4ff86fef0627862170982d69
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-04-15 13:15:48 +0530

    Did URL Conf., models,model form

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
+
+
+class MyModel(models.Model):
+    """model for database"""
+    task = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.task
+
+
+class MyForm(ModelForm):
+    """form for respective model"""
+
+    class Meta:
+        model = MyModel
+        fields = ['task']
